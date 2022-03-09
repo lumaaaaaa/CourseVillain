4f4fd02688275921987655d72773c2644e015a7d
luma <admin@luma.pw>
2022-03-09 14:09:20 -0500

    Unblur by combining different split images

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 using RestSharp;
 using System;
 using System.IO;
+using System.Threading;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 
 namespace CourseVillain
 {
@@ -23,9 +26,24 @@ namespace CourseVillain
             Console.Write("[?] Enter the URL of the document: ");
             string url = Console.ReadLine();
             HtmlWeb htmlWeb = new HtmlWeb();
+            htmlWeb.UserAgent = "Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0";
+            htmlWeb.UseCookies = true;
+            htmlWeb.PreRequest += request =>
+            {
+                var headers = request.Headers;
+                headers.Add("Accept-Language", "en-US,en;q=0.5");
+                headers.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8");
+                return true;
+            };
             HtmlDocument doc = htmlWeb.Load(url);
+            if (doc.Text.Contains("Incapsula incident ID"))
+            {
+                Console.WriteLine("[X] Complete the captcha on site in your browser then try again.");
+                Thread.Sleep(5000);
+                Environment.Exit(42);
+            }
             string filehash = Between(doc.Text, "ppt-entry-point\" data-filehash=\"", "\" data-ppt-position=\"document\"");
-            string datarsid = Between(doc.Text, "/splits/", "/split-0-page-1-html-bg.jpg);background-position:0 0&quot;}}");
+            string datarsid = Between(doc.Text, "preloadImages\":[\"/doc-asset/bg/" + filehash +"/splits/", "/split-0-page-1-html-bg.jpg\"],");
             string dirname = Between(doc.Text, "<title>", "</title>").Trim().Replace(" ", "_").Replace("|", "-").Replace(".", "-").Replace(":", "-").Replace("@", "-");
             DirectoryInfo di = Directory.CreateDirectory("out/" + dirname);
             string pagecount;
@@ -49,6 +67,39 @@ namespace CourseVillain
                     byte[] secondarydlresp = download.DownloadData(secondarydlreq);
                     File.WriteAllBytes("out/" + dirname + "/" + "page" + i + "-2.jpg", secondarydlresp);
                     Console.WriteLine("[i] Downloaded 'page" + i + "-2.jpg'");
+                    
+                    using Image<Rgba32> image = Image.Load<Rgba32>("out/" + dirname + "/" + "page" + i + ".jpg");
+                    using Image<Rgba32> image2 = Image.Load<Rgba32>("out/" + dirname + "/" + "page" + i + "-2.jpg");
+                    Rgba32[] pixelArray = new Rgba32[image.Width * image.Height];
+                    image.CopyPixelDataTo(pixelArray);
+                    Rgba32[] pixelArray2 = new Rgba32[image2.Width * image2.Height];
+                    Rgba32[] outArray = new Rgba32[image2.Width * image2.Height];
+                    image2.CopyPixelDataTo(pixelArray2);
+                    for (int j = 0; j < pixelArray.Length; j++)
+                    {
+                        if (pixelArray[j].R == pixelArray2[j].R)
+                        {
+                            outArray[j] = pixelArray[j];
+                        }
+                        else
+                        {
+                            if (pixelArray[j].R > pixelArray2[j].R)
+                            {
+                                outArray[j] = pixelArray2[j];
+                            }
+                            else
+                            {
+                                outArray[j] = pixelArray[j];
+                            }
+                        }
+                    }
+                    using (var img = Image.LoadPixelData(outArray, image2.Width, image2.Height))
+                    {
+                        File.Delete("out/" + dirname + "/" + "page" + i + "-2.jpg");
+                        File.Delete("out/" + dirname + "/" + "page" + i + ".jpg");
+                        img.Save("out/" + dirname + "/" + "page" + i + ".jpg");
+                        Console.WriteLine("[i] Merged 'page" + i + ".jpg'");
+                    }
                 }
                 else
                 {
@@ -57,8 +108,9 @@ namespace CourseVillain
                 }
                 
             }
-            Console.WriteLine("[i] Finished downloading " + pagecount + " pages! Saved to " + di.FullName);
-            Console.WriteLine("--------------\nPress any key to exit...");
+            
+            Console.WriteLine("[i] Finished downloading " + pagecount + " pages! Saved to '" + di.FullName + "'");
+            Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
     }
